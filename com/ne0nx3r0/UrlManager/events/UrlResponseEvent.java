6a0da0b0004618bcc1c6a83618c200d6ecd38e1b
Ne0n x3r0 <ne0nx3r0@gmail.com>
2012-05-03 22:02:01 -0500

    Added getJSONResult method to UrlResponseEvent (JSON support)

--- com/ne0nx3r0/UrlManager/events/UrlResponseEvent.java
+++ com/ne0nx3r0/UrlManager/events/UrlResponseEvent.java
@@ -1,9 +1,15 @@
 package com.ne0nx3r0.UrlManager.events;
 
 import com.ne0nx3r0.UrlManager.UrlCall;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
  
 public class UrlResponseEvent extends Event {    
     private static final HandlerList handlers = new HandlerList();
@@ -22,6 +28,20 @@ public class UrlResponseEvent extends Event {
  
     public String getPlainTextResult(){
         return this.result;
+    }
+    
+    public Map<String,String> getJSONResult(){
+        JSONObject json;
+        
+        try {
+            json = (JSONObject) new JSONParser().parse(this.result);
+            
+            return (Map<String,String>) json;
+        } catch (ParseException ex) {
+            Logger.getLogger(UrlResponseEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
     }
     
     public Player getSender(){
